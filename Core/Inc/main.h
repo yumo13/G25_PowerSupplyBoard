463f2ab62e071be0f4b65f15509c67ef19003e56
yumo13 <yumo13.yk@gmail.com>
2025-09-21 02:37:49 +0900

    Add pin configs

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,6 +57,16 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define EMERGENCY_Pin GPIO_PIN_0
+#define EMERGENCY_GPIO_Port GPIOA
+#define CAN_STBY_Pin GPIO_PIN_10
+#define CAN_STBY_GPIO_Port GPIOA
+#define LED_B_Pin GPIO_PIN_4
+#define LED_B_GPIO_Port GPIOB
+#define LED_G_Pin GPIO_PIN_5
+#define LED_G_GPIO_Port GPIOB
+#define LED_R_Pin GPIO_PIN_7
+#define LED_R_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
